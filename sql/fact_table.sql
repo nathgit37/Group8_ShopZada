7c500f0d3efb14d88cee3dbf14210581fb4c563c
nathgit37 <nathanemmanuelreyes@gmail.com>
2025-12-05 16:09:21 +0800

    Initialize fact_order table creation script
    This script establishes the structure for the Facts table, ensuring data integrity by preventing null values and utilizing joins.

---
+++ sql/fact_table.sql
@@ -0,0 +1,60 @@
+"""
+============================================
+Creating the Facts Table
+============================================
+Script Purpose:
+    This is a SQL script that creates the tables for our 
+    Facts table based on our outline utilizing joins and
+    ensuring no null values are in the Fact Table.
+============================================
+"""
+
+CREATE TABLE fact_order AS
+SELECT
+	ROW_NUMBER () OVER (ORDER BY o.order_id) AS order_reference_number,
+	u.user_reference_number,
+	CASE 
+		WHEN product.product_reference_number IS NULL
+		THEN -1
+		ELSE product.product_reference_number END AS product_reference_number,
+	CASE 
+		WHEN campaign.campaign_reference_number IS NULL 
+		THEN -1 
+		ELSE campaign.campaign_reference_number END AS campaign_reference_number,
+	staff.staff_reference_number,
+	merchant.merchant_reference_number,
+	o.order_id,
+	o.estimated_arrival, 
+	o.transaction_date,
+	COALESCE(l1.price, 0) AS price,
+	COALESCE(l1.quantity, 0) AS quantity,
+	COALESCE(d."delay in days", 0) AS delay_in_days,
+	CASE
+		WHEN t.availed LIKE '1' 
+		THEN 'Availed'
+		WHEN t.availed LIKE '0'
+		THEN 'Not Availed'
+		ELSE 'Not Applicable' END as availed
+FROM order_data AS o
+LEFT JOIN line_item_data_prices AS l1	
+ON o.order_id = l1.order_id
+LEFT JOIN line_item_data_products AS l2
+ON o.order_id = l2.order_id
+LEFT JOIN order_delays AS d
+ON o.order_id = d.order_id
+LEFT JOIN transactional_campaign_data AS t
+ON o.order_id = t.order_id
+LEFT JOIN dim_user AS u
+ON o.user_id = u.user_id
+LEFT JOIN dim_product AS product
+ON l2.product_id = product.product_id
+LEFT JOIN dim_campaign AS campaign
+ON t.campaign_id = campaign.campaign_id 
+LEFT JOIN merchant_with_order_data AS oms
+ON o.order_id = oms.order_id
+LEFT JOIN dim_staff AS staff
+ON oms.staff_id = staff.staff_id
+LEFT JOIN dim_merchant AS merchant
+ON oms.merchant_id = merchant.merchant_id;
+
+
